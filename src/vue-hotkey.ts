00e204518e85ab44854bdeaceafa431045187a69
rameel <rameel-b@hotmail.com>
2024-02-01 02:22:02 +0600

    Add hotkey property to determinie which of hotkeys triggered the event

--- src/vue-hotkey.ts
+++ src/vue-hotkey.ts
@@ -3,7 +3,18 @@ import { registerHotkey } from "@ramstack/hotkey";
 
 const optionKeys = ["stop", "passive", "prevent", "once", "capture", "window", "document"];
 
-export const vHotkey: ObjectDirective<HTMLElement, (e: KeyboardEvent) => void> = {
+/**
+ * Represents an event triggered by a hotkey.
+ * Extends the standard {@link KeyboardEvent} to include additional {@link hotkey} property.
+ */
+interface HotkeyEvent extends KeyboardEvent {
+    /**
+     * Gets the hotkey that triggered this event.
+     */
+    readonly hotkey: string;
+}
+
+export const vHotkey: ObjectDirective<HTMLElement, (e: HotkeyEvent) => void> = {
     mounted(el, { arg, modifiers, value }) {
         const {
             stop,
@@ -24,6 +35,7 @@ export const vHotkey: ObjectDirective<HTMLElement, (e: KeyboardEvent) => void> =
                 function(e) {
                     stop && e.stopPropagation();
                     prevent && e.preventDefault();
+                    e["hotkey"] = hotkey;
                     value?.call(this, e);
                 },
                 arg ?? "keydown",
